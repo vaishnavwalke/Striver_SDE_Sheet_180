e453177e60e534e373883c1928aa58d8d4059c9e
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2022-06-07 14:35:07 +0530

    Update 1.Set Matrix Zeroes.cpp

--- 1.Set Matrix Zeroes.cpp
+++ 1.Set Matrix Zeroes.cpp
@@ -56,3 +56,35 @@ public:
 
 
 //APPROACH -2 
+//Time Complexity - O(n*m) + O(n*m)
+//Space Complexity - O(n)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<int> dummy1(rows,-1), dummy2(cols,-1);
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(matrix[i][j] == 0){
+                    dummy1[i] = 0;
+                    dummy2[j] = 0;
+                }
+            }
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(dummy1[i] == 0 || dummy2[j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+
+//APPROACH -3 
+//Time Complexity - O(2*n*m)
+//Space Complexity - O(1)
+
