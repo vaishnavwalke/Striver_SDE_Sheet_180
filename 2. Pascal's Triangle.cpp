15d3a0ccaa998f3b23309944eac824bb13970354
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2022-06-08 16:32:51 +0530

    Create 2. Pascal's Triangle.cpp

---
+++ 2. Pascal's Triangle.cpp
@@ -0,0 +1,18 @@
+// Time Complexity = O(numRows * numRows)
+// Space Complexity = O(numRows * numRows)
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+        
+        for(int i = 0; i < numRows; i++){
+            r[i].resize(i+1);    // increasing columns for each row
+            r[i][0] = r[i][i] = 1;   // setting first and last element of row to 1
+            for(int j = 1; j< i; j++){
+                r[i][j] = r[i-1][j-1] + r[i-1][j];
+            }
+        }
+        return r;
+    }
+};
